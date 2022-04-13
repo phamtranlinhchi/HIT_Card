9dc12971cf808589435d0333f055a7e373b7a4aa
izme <91523388+phamtranlinhchi@users.noreply.github.com>
2022-04-14 01:48:29 +0700

    update render card

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import { exportComponentAsPNG } from 'react-component-export-image';
-import React, { useRef } from 'react';
+import React from 'react';
 import Card from './Card';
-
-const user = {
-    avatar: './avatar.jpeg',
-    name: 'PHẠM TRẦN LINH CHI',
-    class_year: 'KTPM01-K15',
-    qr_code: './qrcode.png',
-};
+import users from './data.json';
 
 function App() {
-    const componentRef = useRef();
     return (
-        <div className='App'>
-            <Card user={user} ref={componentRef} />
-            <button onClick={() => exportComponentAsPNG(componentRef)}>
-                Export As PNG
-            </button>
+        <div
+            className='App'
+            style={{
+                display: 'flex',
+                flexWrap: 'wrap',
+            }}
+        >
+            {users && users.map((user) => <Card user={user} />)}
         </div>
     );
 }
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,25 +1,36 @@
-import React from 'react';
+
+import { exportComponentAsPNG } from 'react-component-export-image';
+import React, {useRef} from 'react';
 import './Card.css'
 
-const Card =  React.forwardRef(function ({user}, ref) {
+const Card =  React.forwardRef(function ({user}) {
+    const componentRef = useRef();
+
+    const splitName = user.full_name.split(' ');
+    const name = splitName[splitName.length - 1];
+
     return (
-    
-        <div className="card" ref={ref}>
-            <div className="bgcard">
-                <img src="./bg.png" alt="backgroundcard" />
+        <div className="container">
+            <div className="card" ref={componentRef}>
+                <div className="bgcard">
+                    <img src="./bg.png" alt="backgroundcard" />
+                </div>
+                <div className="avatar">
+                    <img src={`api.hithaui.com${user.avatar}`} alt="avatar" />
+                </div>
+                <div className="name">
+                    {user.full_name.toUpperCase()}
+                </div>
+                <div className="class-year">
+                    {`${user.class_name}-K${user.school_year}`}
+                </div>
+                <div className="qrcode">
+                    <img src={`api.hithaui.com${user.qr_code}`} alt="qrcode" />
+                </div>
             </div>
-            <div className="avatar">
-                <img src={user.avatar} alt="avatar" />
-            </div>
-            <div className="name">
-                {user.name}
-            </div>
-            <div className="class-year">
-                {user.class_year}
-            </div>
-            <div className="qrcode">
-                <img src={user.qr_code} alt="qrcode" />
-            </div>
+            <button onClick={() => exportComponentAsPNG(componentRef)}>
+                Export {name} As PNG
+            </button>
         </div>
     )
 })
